62bc7aeb6265ca81e7d272d2bada7b8dbc4483a3
Mohammed Abiola <yayahmohammmed@gmail.com>
2018-04-18 00:50:41 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG internship stage 1</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="main">
+		<div class="header"><h1>HNG Internship 4.0</h1></div>
+		<div class="time"><p class="t"><?php echo  date("g:i a", time()); ?></p></div>
+		<div class="footer"><div class="b"><strong>Thanks to</strong></div><div class="sponsors"><img src="sponsors.jpg" width="500px" height="87px"></div></div>
+	</div>
+</body>
+</html>
